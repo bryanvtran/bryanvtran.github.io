74536298af2475f62bafb072e91efafe435d39be
Bryan Van Tran <Bryan.V.Tran@aexp.com>
2017-08-21 17:34:36 -0700

    finish photo page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 var photos = document.querySelectorAll('.photos__item');
 var viewer = document.querySelector('.photo-viewer');
+var photosNavItems = document.querySelectorAll('.photos-nav__item');
 
 photos.forEach(function(photo) {
   photo.addEventListener('click', function() {
@@ -21,3 +22,37 @@ document.addEventListener('keyup', function(e) {
     viewer.style.display = 'none';
   }
 });
+
+photosNavItems.forEach(function(link) {
+  link.addEventListener('click', function(e) {
+    var tag = this.innerHTML.toLowerCase();
+
+    // removing old active tag and adding new active tag
+    document.querySelectorAll('.photos-nav__item.active').forEach(function(active) {
+      active.classList.remove('active');
+    });
+    this.classList.add('active');
+
+    if (tag === 'all') {
+      // show all
+      photos.forEach(function(photo) {
+        photo.style.display = 'block';
+      });
+    }
+    else {
+      // show only the ones with these classes?
+      photos.forEach(function(photo) {
+        if (hasClass(photo, tag)) {
+          photo.style.display = 'block';
+        }
+        else {
+          photo.style.display = 'none';
+        }
+      });
+    }
+  });
+})
+
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
